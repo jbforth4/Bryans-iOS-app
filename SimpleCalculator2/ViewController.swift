d970d5adaf9a2a8d895bc485cbcf419b10d13b71
Connie Forth <connieforth@2600-6c42-7900-14dd-c8b1-9fad-583b-6970.dhcp6.chtrptr.net>
2018-06-25 18:10:50 -0500

    Initial Commit

---
+++ SimpleCalculator2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCalculator2
+//
+//  Created by Connie Forth on 6/25/18.
+//  Copyright © 2018 Connie Forth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
